95bc1fdc0d2c2022a2057047e587c4fb01a00a9a
Dan7spirit <dass13heat@gmail.com>
2018-06-06 18:09:24 -0600

    Inicio del controlador y vista del jugador.

---
+++ ProyectoSanchez/Controllers/JugadorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSanchez.Controllers
+{
+
+
+    public class JugadorController : Controller
+    {
+        // GET: Jugador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
